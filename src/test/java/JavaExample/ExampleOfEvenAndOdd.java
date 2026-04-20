53034f98bd80ec6e53cdaf91a5699cc9ad09c751
RohitSathe332 <rohitsathe332@gmail.com>
2026-04-20 15:24:11 +0530

    Update ExampleOfEvenAndOdd.java

--- src/test/java/JavaExample/ExampleOfEvenAndOdd.java
+++ src/test/java/JavaExample/ExampleOfEvenAndOdd.java
@@ -3,7 +3,7 @@ package JavaExample;
 public class ExampleOfEvenAndOdd {
 
 	public static void main(String[] args) {
-		int num = 7; // Change this number
+		int num = 16; // Change this number
 
 		if (num % 2 == 0) {
 			System.out.println(num + " is Even");
